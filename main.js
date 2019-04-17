54b57fa9de732e14d96f5b0e39233d3327a2b61d
Emily <lalonde0902@gmail.com>
2019-04-17 08:42:16 -0600

    Add title to card

--- main.js
+++ main.js
@@ -13,24 +13,28 @@ var taskField = document.querySelector('.task-field')
 var taskManager = document.querySelector('.task-manager');
 var storageArray = JSON.parse(localStorage.getItem('taskArray')) || [];
 var taskList = document.querySelector('.task-list');
-var taskField = document.querySelector(".task-field")
 var tasks = document.querySelector('.tasks');
-var deleteListItem = document.querySelector('.delete-button')
+var deleteListItem = document.querySelector('.delete-button');
 
-
+window.addEventListener('load', restoreIdeas);
 window.addEventListener('load', disableButtons);
 window.addEventListener('load', enableButtons);
 titleInput.addEventListener('keyup', disableButtons)
 titleInput.addEventListener('keyup', enableButtons)
 itemInput.addEventListener('keyup', disableButtons)
 itemInput.addEventListener('keyup', enableButtons)
-populateCardButton.addEventListener('click', populateCard)
-taskList.addEventListener('click', function(e) {
+populateCardButton.addEventListener('click', createTask)
+addItemButton.addEventListener('click', createNewListItems);
+taskList.addEventListener('click', function (e) {
   if (e.target.className === 'delete-button') {
     e.target.parentElement.remove();
+    var removedTask = new ToDoList();
+
+    var targetId = parseInt(e.target.parentElement.parentElement.dataset.id);
+    removedTask.deleteFromStorage(targetId);
   }
 });
-taskField.addEventListener('click', function(e) {
+taskField.addEventListener('click', function (e) {
   if (e.target.className === 'delete-card-button') {
     e.target.parentElement.parentElement.parentElement.remove();
   }
@@ -38,18 +42,18 @@ taskField.addEventListener('click', function(e) {
 
 
 function disableButtons(e) {
-  if(titleInput.value ==="" && itemInput.value === ""){
-  addItemButton.disabled = true;
-  addItemButton.classList.add('disabled');
-  populateCardButton.disabled = true;
-  populateCardButton.classList.add('disabled');
-  clearCardsButton.disabled = true;
-  clearCardsButton.classList.add('disabled');
+  if (titleInput.value === "" && itemInput.value === "") {
+    addItemButton.disabled = true;
+    addItemButton.classList.add('disabled');
+    populateCardButton.disabled = true;
+    populateCardButton.classList.add('disabled');
+    clearCardsButton.disabled = true;
+    clearCardsButton.classList.add('disabled');
   }
 };
 
-function enableButtons(e){
-  if(titleInput.value !=="" && itemInput.value !== ""){
+function enableButtons(e) {
+  if (titleInput.value !== "" && itemInput.value !== "") {
     addItemButton.disabled = false;
     addItemButton.classList.remove('disabled');
     populateCardButton.disabled = false;
@@ -60,30 +64,32 @@ function enableButtons(e){
 };
 
 function clearTitleInput() {
-  titleInput.value = "";
+  titleInput.value = '';
 }
 
 function clearItemInput() {
-  itemInput.value = "";
+  itemInput.value = '';
 }
 
-addItemButton.addEventListener('click', createNewTask);
+function clearSideBar() {
+  taskField.innerHTML = '';
+}
 
-function createNewTask(e) {
-	taskList.innerHTML += 
-		`<div class="sidebar-item">
+function createNewListItems() {
+  taskList.innerHTML +=
+    `<div class="sidebar-list-item">
 			<img src="images/delete.svg" class="delete-button">
 			<li class="tasks" data-id="${Date.now()}">${itemInput.value}</li>
-		</div>`
-		localStorage.setItem('tasks', JSON.stringify(tasks))
-    clearItemInput();
-    clearTitleInput();
+    </div>`
+  clearItemInput();
 }
 
-function populateCard (newTasks) {
+
+//map over newTasks to create 
+function populateCard(newTasks) {
   var newCard =
-`<article class = "card" data-id= ${newTasks.id}>
-      <h2> contentEditable="true" class="editable-title">${newTasks.title}</h2>
+    `<article class = "card" data-id="${newTasks.id}">
+      <h2 class="editable-title">${newTasks.title}</h2>
       <section class = "task-list-card">
       </section>
       <footer>
@@ -95,5 +101,39 @@ function populateCard (newTasks) {
         </a>
       </footer>
     </article>`
-    taskField.insertAdjacentHTML('afterbegin', newCard);
+  taskField.insertAdjacentHTML('afterbegin', newCard);
+};
+
+function findListItems(tasks){
+  var gotListItems ='';
+  for(var i = 0; i < tasks.items.length; i++) {
+    gotListItems +=
+    `<li class="list-item">
+    <input type="checkbox" class="done-icon" data-id=${tasks.items[i].id} id=index${i}>
+    <label for="id=index${i}" class="card-todo-item">${tasks.items[i].content}</label>
+    </li>`
+  }
+ return gotListItems;
+};
+
+function instantiateTask(newTask) {
+  var newTask = new ToDoList (titleInput.value, Date.now(), itemInput.value);
+  storageArray.push(newTask);
+  newTask.saveToStorage(storageArray);
+  return newTask;
+};
+
+function createTask () {
+var task = instantiateTask();
+populateCard(task);
 }
+
+
+
+function restoreIdeas() {
+  storageArray = storageArray.map(function(oldTask) {
+    var restoredTask = new Task(oldTask.title, oldTask.id, oldTasks.task);
+    populateCard(restoredIdea);
+    return restoredTask;
+  });
+};
--- todo-list.js
+++ todo-list.js
@@ -1,23 +1,25 @@
 class ToDoList {
-  constructor(id, title, tasks) {
-  this.id= id;
-  this.title= title;
-  this.urgent = false;
-  this.tasks= tasks ||[];
+  constructor(title, id, tasks, urgent=false) {
+    this.title = title;
+    this.id = id;
+    this.tasks = tasks || [];
+    this.urgent = urgent || false;
+    this.urgentImg;
+    this.urgent ? this.urgentImg = 'images/urgent-active.svg' : this.urgentImg = 'images/urgent.svg'
   }
+
+  saveToStorage(newTask) {
+    var stringifiedNewTask = JSON.stringify(newTask);
+    localStorage.setItem('storageArray', stringifiedNewTask);
+  };
+
+  deleteFromStorage(targetId) {
+    var parsedItems = JSON.parse(localStorage.getItem('storageArray'));
+    var itemIndex = parsedItems.findIndex(function (tasks) {
+      return tasks.id === targetId;
+    });
+
+    parsedItems.splice(itemIndex, 1);
+    localStorage.setItem('storageArray', JSON.stringify(parsedItems));
+  };
 }
-
-// saveToStorage() ;{ 
-//   var stringifiedNewTask = JSON.stringify(ideaArray);
-//   localStorage.setItem('taskArray', stringifiedNewTask);
-//   };
-
-//   deleteFromStorage(targetId) ;{
-//   var parsedItems = JSON.parse(localStorage.getItem('taskArray'));
-//   var itemIndex = parsedItems.findIndex(function(tasks) {
-//     return idea.id === targetId;
-//   });
-
-//   parsedItems.splice(itemIndex, 1);
-//   localStorage.setItem('taskArray', JSON.stringify(parsedItems));
-//   };
